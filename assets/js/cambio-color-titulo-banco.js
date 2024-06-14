833fb927756c930cf82765d2519a8418d16d8df1
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-06-14 04:31:16 -0400

    :bug: profile does not show updated image, reservation does not update and room does not identify availability

---
+++ assets/js/cambio-color-titulo-banco.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //codigo de funcion se ejecuta cuando pagina este cargada
+          
+        // Cambia color titulo formulario super hero
+        $("#banco-titulo").dblclick(function () 
+        {
+            $(this).css("color", "red");
+        });    
+              
+  
+    });
+  
